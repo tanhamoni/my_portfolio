275cd9d2f4d0c93b073b757fdd69445bddabf5f9
tanhamoni <tasniya.bd26@gmail.com>
2026-09-13 19:33:27 +0600

    fix entrypoint migration command

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,12 @@
 mkdir -p /var/www/html/database
 touch /var/www/html/database/database.sqlite
 
-# Set writable permissions for Laravel
+# Set permissions
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache /var/www/html/database
 chmod -R 777 /var/www/html/storage /var/www/html/bootstrap/cache /var/www/html/database /var/www/html/database/database.sqlite
 
-# Run Laravel migrations with force & seed
+# Run migrations inside Render Docker Container
 php artisan migrate:fresh --seed --force
 
-# Start Apache in foreground
+# Start Apache
 exec apache2-foreground
