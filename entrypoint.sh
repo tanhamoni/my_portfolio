1352d952825d1dba88f7a3f1dd0b5ad4f5e895ba
tanhamoni <tasniya.bd26@gmail.com>
2026-09-13 20:49:19 +0600

    fix valid sqlite DB initialization

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-mkdir -p /var/www/html/storage/logs
+
+# Folder & log permissions
+mkdir -p /var/www/html/storage/logs /var/www/html/storage/framework/views /var/www/html/storage/framework/sessions /var/www/html/storage/framework/cache /var/www/html/database
+
+# Delete corrupt file and create clean sqlite file
+rm -f /var/www/html/database/database.sqlite
+php -r "new PDO('sqlite:/var/www/html/database/database.sqlite');"
+
+# Set permissions
 chmod -R 777 /var/www/html/storage /var/www/html/bootstrap/cache /var/www/html/database
+
+# Force clean migration and seed
+php artisan migrate:fresh --seed --force
+
+# Start Apache
 exec apache2-foreground
