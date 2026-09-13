d977a9f250b63b556e58aa1bb178ddebb7af0aa1
tanhamoni <tasniya.bd26@gmail.com>
2026-09-13 19:59:38 +0600

    add fresh migration to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Ensure storage structure
 mkdir -p /var/www/html/storage/logs
+
+# Set full permissions
 chmod -R 777 /var/www/html/storage /var/www/html/bootstrap/cache /var/www/html/database
+
+# Run migration & seed directly before starting Apache
+php artisan migrate:fresh --seed --force
+
+# Start Apache
 exec apache2-foreground
